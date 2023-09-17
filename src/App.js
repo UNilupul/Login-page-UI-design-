e0ff4cbc1fa9579157e6ba06a1b6941972ec9528
UNilupul <udaran97@gmail.com>
2023-09-17 11:13:59 +0530

    set routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 
 import './App.css';
+import Home from './home/home';
 import Login from './login/login';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+      <BrowserRouter>
+        <Routes>
+          <Route  path='/' element = {<Login/>} />
+          <Route  path='/home' element = {<Home/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
